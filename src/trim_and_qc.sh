917dd7f9bf33b86385fd9869a128376447d5adcb
Sitara Cullinan <Sitara.Cullinan@uga.edu>
2025-05-31 15:13:42 -0400

    Removed raw QC code to keep in separate script to keep steps logically modular (raw QC alone then check output then trim plus QC on trimmed reads and check that separately afterward).

---
+++ src/trim_and_qc.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=capstone_trim_and_qc
+#SBATCH --ntasks=1
+#SBATCH --time=0:30:00
+#SBATCH --mem=2gb
+#SBATCH --output=/scratch/sjc78466/capstone/job_logs/log.%j
+
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=sjc78466@uga.edu
+
+##########################################################################
+###   LOADING ALL SOFTWARE MODULES NEEDED FOR TRIMMING AND QC CHECK    ###
+##########################################################################
+
+module load FastQC/0.11.9-Java-11
+module load MultiQC/1.14-foss-2022a
+module load Trimmomatic/0.39-Java-13
+
+##############################
+###   DEFINING VARIABLES   ###
+##############################
+
+# Variable for absolute path again so don't need to keep typing it out
+workdir="/scratch/sjc78466/capstone"
+
+# Variable for Trimmomatic program
+TRIMMOMATIC="java -jar /apps/eb/Trimmomatic/0.39-Java-13/trimmomatic-0.39.jar"
+
+######################################################
+### TRIM OFF ADAPTERS AND LOW QUALITY BASE CALLS   ###
+######################################################
+
+
+
+############################################################
+###   RUNNING FASTQC ON TRIMMED READS TO CHECK QUALITY   ###
+############################################################
+
+
+
+
+####################################################################
+###   RUNNING MULTIQC TO COMPILE QC RESULTS INTO SINGLE REPORT   ###
+####################################################################
+
+# Moving MultiQC HTML file from cluster to local computer using FileZilla to visualize
+
+
+
+
